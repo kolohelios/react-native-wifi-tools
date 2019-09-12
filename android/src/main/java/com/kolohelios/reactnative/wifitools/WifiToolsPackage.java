315a47687efefc4561e9d2509a6d6cc3c3f73dda
Jon Edwards <jon@buddy.com>
2019-09-12 07:09:05 -0700

    copy over core files from react-native-iot-wifi and adjust to get it working

--- android/src/main/java/com/kolohelios/reactnative/wifitools/WifiToolsPackage.java
+++ android/src/main/java/com/kolohelios/reactnative/wifitools/WifiToolsPackage.java
@@ -3,12 +3,13 @@ package com.kolohelios.reactnative.wifitools;
 import java.util.Arrays;
 import java.util.Collections;
 import java.util.List;
-
+import java.util.ArrayList;
 import com.facebook.react.ReactPackage;
+import com.facebook.react.bridge.JavaScriptModule;
 import com.facebook.react.bridge.NativeModule;
 import com.facebook.react.bridge.ReactApplicationContext;
 import com.facebook.react.uimanager.ViewManager;
-import com.facebook.react.bridge.JavaScriptModule;
+import android.app.Activity;
 
 public class WifiToolsPackage implements ReactPackage {
     @Override
--- ios/WifiTools.h
+++ ios/WifiTools.h
@@ -1,5 +1,7 @@
+#import <Foundation/Foundation.h>
 #import <React/RCTBridgeModule.h>
 
 @interface WifiTools : NSObject <RCTBridgeModule>
 
 @end
+
