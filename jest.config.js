688f57677d92768369f2efbaffe92884ceb9bfdf
Jon Edwards <jkedwards@me.com>
2019-12-24 09:55:35 -0800

    add diagnostics for ts-jest create meaningful Jest test for interface add allprojects section to build.gradle change permissisons for android/gradlew update Travis script to experiment with testing a native build

--- jest.config.js
+++ jest.config.js
@@ -2,4 +2,9 @@ module.exports = {
 	preset: 'react-native',
 	collectCoverageFrom: ['src/**/*.ts'],
 	transform: { '^.+\\.(ts|tsx)$': 'ts-jest' },
+	globals: {
+		'ts-jest': {
+			diagnostics: true,
+		},
+	},
 };
--- src/index.spec.ts
+++ src/index.spec.ts
@@ -1,5 +1,23 @@
-describe('test react-native-wifi-tools', () => {
-	it('should always pass', () => {
-		expect(true).toBeTruthy();
+import { NativeModules } from 'react-native';
+import wifi from './index';
+
+describe('test react-native-wifi-tools interface', () => {
+	beforeAll(() => {
+		NativeModules.WifiTools = {
+			isApiAvailable: _callback => undefined,
+		};
+	});
+
+	it('should have same function properties exposed', () => {
+		expect(wifi.connect).toBeDefined();
+		expect(wifi.connectSecure).toBeDefined();
+		expect(wifi.getSSID).toBeDefined();
+		expect(wifi.isApiAvailable).toBeDefined();
+		expect(wifi.removeSSID).toBeDefined();
+	});
+
+	it('should have isAPIAvailable with same signature', () => {
+		const isApiAvailable = wifi.isApiAvailable();
+		expect(isApiAvailable).toBe(undefined);
 	});
 });
